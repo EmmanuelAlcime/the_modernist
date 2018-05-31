8ef4e9a3f3c37e8703b45e77d1f41c5c63c86f32
emmanuelalcime <emmanuelalcime54@gmail.com>
2018-05-31 19:38:51 -0400

    hero div setup up completed without responsiveness and functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,26 @@
 ;(function(global){
     'use strict';
 
+     // slider navigation button
+     var previous = doucment.getElementById("previous");
+     var next = doucment.getElementById("next");
+     
+     // click event listeners functions 
+     next.addEventListener('click' , (evt)=>{
+      evt.preventDefault();
+      nextDiv();
+     } , flase);
+
+
+     next.addEventListener('click' , (evt)=>{
+       evt.preventDefault();
+     } , flase);
+
+     
+     function nextDiv(){
+     }
+
+     function previousDiv(){
+     }
     
 })(window);
