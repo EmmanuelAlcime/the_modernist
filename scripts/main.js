9411b40479dcd147d4d77c7e6da465e6e16009fc
emmanuelalcime <emmanuelalcime54@gmail.com>
2018-06-01 08:11:31 -0400

    completed project without mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,56 @@
-;(function(global){
+;(function(){
     'use strict';
 
      // slider navigation button
-     var previous = doucment.getElementById("previous");
-     var next = doucment.getElementById("next");
-     
+     var previous = document.getElementById("previous");
+     var next = document.getElementById("next");
+     var sliderDivs = document.getElementsByClassName("hero--slider__sliderShow");
+     var index_val = 0; 
+
+     setInterval(nextDiv,3000);
+
      // click event listeners functions 
      next.addEventListener('click' , (evt)=>{
       evt.preventDefault();
       nextDiv();
-     } , flase);
+     } , false);
 
 
-     next.addEventListener('click' , (evt)=>{
+     previous.addEventListener('click' , (evt)=>{
        evt.preventDefault();
-     } , flase);
+       previousDiv();
+     } , false);
 
      
      function nextDiv(){
-     }
+       console.log(index_val);
+       if( index_val <  sliderDivs.length){
+           index_val++;
+        
+           sliderDivs['mainCaption'].innerHTML = sliderDivs[index_val].innerHTML;
+           console.log(sliderDivs['mainCaption']);   
+            console.log(index_val);   
+        }
+        else if(index_val > 1){
+          index_val = 0;
+          sliderDivs['mainCaption'].innerHTML = sliderDivs[0].innerHTML;
+          console.log("index_val is greater");
+        }
+      }
+
 
      function previousDiv(){
+       index_val--;
+       console.log(index_val);
+       if (index_val >= 0){
+         sliderDivs['mainCaption'].innerHTML = sliderDivs[index_val].innerHTML;
+         console.log(sliderDivs['mainCaption']);      
+       }
+       else{
+      
+        index_val= sliderDivs.length - 1;
+        sliderDivs['mainCaption'].innerHTML = sliderDivs[index_val].innerHTML;
+       } 
      }
     
 })(window);
---
+++ scripts/nav.js
@@ -0,0 +1,15 @@
+;(function(){
+    
+$(document).ready(function(){
+  $(window).scroll(function(){
+   var scroll = $(window).scrollTop(); 
+   if( scroll > 0 ){
+      $(".header").addClass("fixed");
+     
+   }else{
+      $(".header").removeClass("fixed");
+    }
+  });
+console.log(scroll);
+  });
+})();
